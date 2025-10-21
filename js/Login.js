dde048d82fef231057de327a07470f3d31e945e9
rafaelduclos-stack <rafael.duclos@escola.pr.gov.br>
2025-10-21 20:04:27 -0300

    initial commit

---
+++ js/Login.js
@@ -0,0 +1,7 @@
+// Faz todos os botões de inscrição redirecionarem para Login.html
+const botoes = document.querySelectorAll('.inscrever-btn');
+botoes.forEach(btn => {
+  btn.addEventListener('click', () => {
+    window.location.href = '../pages/Login.html';
+  });
+});
