bd9ca0cf344845add1f0843fc519e846c1beb254
rafaelduclos-stack <rafael.duclos@escola.pr.gov.br>
2025-10-21 21:01:32 -0300

    Delete Login.js

--- Login.js
+++
@@ -1,7 +0,0 @@
-// Faz todos os botões de inscrição redirecionarem para Login.html
-const botoes = document.querySelectorAll('.inscrever-btn');
-botoes.forEach(btn => {
-  btn.addEventListener('click', () => {
-    window.location.href = 'Login.html';
-  });
-});
